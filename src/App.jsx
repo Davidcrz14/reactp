633424b42044230bf80264f24a0beecbe74fc103
davc <cucd060823@gs.utm.mx>
2024-11-09 08:21:52 -0600

    analisis

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Analytics } from "@vercel/analytics/react";
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Skills from './components/Skills';
@@ -20,6 +21,7 @@ function App() {
         <Certificates />
       </main>
       <Footer />
+      <Analytics />
     </div>
   );
 }
