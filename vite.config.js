6dcb8faec89aee1e2eab88666ffff30540b6ec12
davc <cucd060823@gs.utm.mx>
2024-11-09 06:32:26 -0600

    prueba2

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,10 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  server: {
-    port: 3000,
-    host: true
-  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  }
+    sourcemap: true,
+    emptyOutDir: true,
+  },
 })
