1bc258541737aecb4a14c305633cf9e550b0c555
Davidcrz14 <davidprofesor14@gmail.com>
2024-12-24 16:36:15 -0600

    vercel2

--- vite.config.js
+++ vite.config.js
@@ -25,13 +25,8 @@ export default defineConfig({
       }
     },
     cssCodeSplit: true,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    }
+    minify: 'esbuild',
+    target: 'esnext'
   },
   publicDir: 'public',
   css: {
